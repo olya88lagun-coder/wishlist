1a28b4c5fdf36661b09d28b87fb94763d264e075
olya88lagun-coder <olya88lagun@mail.ru>
2026-09-20 15:56:58 +0500

    feat(analytics): align Metrika counter with Yandex snippet

--- apps/web/src/components/YandexMetrika.tsx
+++ apps/web/src/components/YandexMetrika.tsx
@@ -10,17 +10,24 @@ export function YandexMetrika() {
       <Script id="yandex-metrika" strategy="afterInteractive">
         {`
           (function(m,e,t,r,i,k,a){
-            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0];
-            k.async=1;k.src=r+"?id="+i;a.parentNode.insertBefore(k,a)
-          })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "${id}");
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+              k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=${id}", "ym");
+
           ym(${id}, "init", {
-            clickmap: true,
-            trackLinks: true,
-            accurateTrackBounce: true,
+            ssr: true,
             webvisor: true,
-            ssr: true
+            clickmap: true,
+            ecommerce: "dataLayer",
+            referrer: document.referrer,
+            url: location.href,
+            accurateTrackBounce: true,
+            trackLinks: true
           });
         `}
       </Script>
