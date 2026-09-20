d6aec3941d143c07564c67323b6502ce8dbea62e
olya88lagun-coder <olya88lagun@mail.ru>
2026-09-20 15:25:45 +0500

    feat(seo): add new gift landing pages to sitemap

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -3,8 +3,8 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   const base = process.env.APP_URL ?? "https://my-wish-list.online";
   const giftSlugs = [
-    "for-mom","for-dad","for-girlfriend","for-boyfriend","for-wife","for-husband","for-friend","for-colleague",
-    "birthday","new-year","wedding","anniversary","under-5000","under-10000","under-20000",
+    "for-mom","for-dad","for-girlfriend","for-boyfriend","for-wife","for-husband","for-friend","for-colleague","for-sister","for-brother","for-grandma","for-grandpa","for-daughter","for-son","for-teacher","for-boss",
+    "birthday","new-year","wedding","anniversary","housewarming","valentines-day","march-8","february-23","secret-santa","under-1000","under-3000","under-5000","under-10000","under-15000","under-20000",
   ];
 
   return [
